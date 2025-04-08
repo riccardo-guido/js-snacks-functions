77400fae0122b88d873f7f53f58c2ea86f32ea8c
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-08 16:40:50 +0200

    esercizio 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,29 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function countVowels(phrase) {
+    let vowelsNumber = 0;
+
+    for( let i = 0; i < phrase.length; i++) {
+        const currentChar = phrase[i];
+
+        if ( currentChar ===  "a" ||
+            currentChar ===  "e" ||
+            currentChar ===  "i" ||
+            currentChar ===  "o" ||
+            currentChar ===  "u" 
+        ) {
+            vowelsNumber += 1;
+            console.log(phrase[i]);
+            
+        }
+    }
+    return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
