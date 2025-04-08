31dd30f77d6c86dc10627dce261d4bd5f3e6e4b0
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-08 15:55:10 +0200

    esercizio 01

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(nome) {
+    console.log("ciao " + nome);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluto(userName)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
